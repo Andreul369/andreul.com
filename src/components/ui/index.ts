df08ee1da80c11d838c323080da68d9d661ef1f9
andreibojor <bojor.andrei@gmail.com>
2024-05-14 10:36:38 +0300

    has commited

---
+++ src/components/ui/index.ts
@@ -0,0 +1,25 @@
+export { Button, buttonVariants } from './button';
+
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './card';
+
+export { Input } from './input';
+
+export { Label } from './label';
+
+export {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from './select';
