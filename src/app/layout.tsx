a5ccade056161b665c915229cbb2bb0f743b6595
andreibojor <bojor.andrei@gmail.com>
2024-05-15 14:03:56 +0300

    dasdsads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="relative flex flex-col px-4">
             <NavBar />
-            <main className="flex min-h-screen flex-col items-center justify-between pt-16">
+            <main className="flex min-h-screen flex-col items-center justify-between pt-14">
               {children}
             </main>
             <Footer />
--- src/components/work.tsx
+++ src/components/work.tsx
@@ -1,7 +1,82 @@
 import React from 'react';
+import Image from 'next/image';
 
-const Works = () => {
-  return <div>Works</div>;
+import * as Icons from '@/components/icons';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Input,
+  Label,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui';
+
+const Work = () => {
+  return (
+    <section className="w-full">
+      <Card>
+        <CardHeader>
+          <CardTitle>DevFlow</CardTitle>
+          <CardDescription>A platform like StackOverflow</CardDescription>
+        </CardHeader>
+        <CardContent className="relative h-96 rounded-lg">
+          <Image
+            src="/project-1.png"
+            alt="project1"
+            // height={200}
+            // width={400}
+            objectFit="cover"
+            fill
+          />
+        </CardContent>
+        <CardFooter className="flex justify-end gap-2">
+          <Button variant="outline">
+            <Icons.LaptopMinimal className="mr-2 size-4" />
+            Demo
+          </Button>
+          <Button variant="outline">
+            <Icons.GitHub className="mr-2 size-4" />
+            Github
+          </Button>
+        </CardFooter>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>DevFlow</CardTitle>
+          <CardDescription>A platform like StackOverflow</CardDescription>
+        </CardHeader>
+        <CardContent className="relative h-96 rounded-lg">
+          <Image
+            src="/project-1.png"
+            alt="project1"
+            // height={200}
+            // width={400}
+            objectFit="cover"
+            fill
+          />
+        </CardContent>
+        <CardFooter className="flex justify-end gap-2">
+          <Button variant="outline">
+            <Icons.LaptopMinimal className="mr-2 size-4" />
+            Demo
+          </Button>
+          <Button variant="outline">
+            <Icons.GitHub className="mr-2 size-4" />
+            Github
+          </Button>
+        </CardFooter>
+      </Card>
+    </section>
+  );
 };
 
-export default Works;
+export default Work;
