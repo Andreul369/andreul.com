93d6dcdc608e39d6b2cc8b425cb108a0e0abfe99
andreibojor <bojor.andrei@gmail.com>
2024-05-16 14:31:36 +0300

    dsdad

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import * as Icons from '@/components/icons';
+import { Button, Card, CardContent, CardHeader, CardTitle } from './ui';
+
 const Socials = () => {
-  return <section>Socials</section>;
+  return (
+    <section>
+      <Card>
+        <CardHeader>
+          <CardTitle>Find me on</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Button variant="outline" className="p-4">
+            <Icons.GitHub className="size-8" />
+          </Button>
+          <Button variant="outline" className="p-4">
+            <Icons.LinkedIn className="size-8" />
+          </Button>
+        </CardContent>
+      </Card>
+    </section>
+  );
 };
 
 export default Socials;
