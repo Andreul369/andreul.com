35b3d497e91c6d577ba3d6c88a0d28c77538936a
andreibojor <bojor.andrei@gmail.com>
2024-05-16 14:20:30 +0300

    asdsad

--- src/components/home-intro.tsx
+++ src/components/home-intro.tsx
@@ -5,9 +5,7 @@ const HomeIntro = () => {
   return (
     <section className="flex min-h-[50vh] flex-col items-center justify-center">
       <Balancer>
-        <h1 className="text-center text-8xl">
-          Let&apos;s bring an idea to a bright future.
-        </h1>
+        <h1 className="text-center text-8xl">A bright future.</h1>
       </Balancer>
       <h1 className="text-center text-8xl">Together.</h1>
     </section>
--- src/components/work.tsx
+++ src/components/work.tsx
@@ -21,50 +21,16 @@ import {
 
 const Work = () => {
   return (
-    <section className="w-full">
+    <section className="flex w-full flex-col gap-4">
       <Card>
         <CardHeader>
           <CardTitle>DevFlow</CardTitle>
           <CardDescription>A platform like StackOverflow</CardDescription>
         </CardHeader>
         <CardContent className="relative h-96 rounded-lg">
-          <Image
-            src="/project-1.png"
-            alt="project1"
-            // height={200}
-            // width={400}
-            objectFit="cover"
-            fill
-          />
+          <Image src="/project-1.png" alt="project1" objectFit="cover" fill />
         </CardContent>
-        <CardFooter className="flex justify-end gap-2">
-          <Button variant="outline">
-            <Icons.LaptopMinimal className="mr-2 size-4" />
-            Demo
-          </Button>
-          <Button variant="outline">
-            <Icons.GitHub className="mr-2 size-4" />
-            Github
-          </Button>
-        </CardFooter>
-      </Card>
-
-      <Card>
-        <CardHeader>
-          <CardTitle>DevFlow</CardTitle>
-          <CardDescription>A platform like StackOverflow</CardDescription>
-        </CardHeader>
-        <CardContent className="relative h-96 rounded-lg">
-          <Image
-            src="/project-1.png"
-            alt="project1"
-            // height={200}
-            // width={400}
-            objectFit="cover"
-            fill
-          />
-        </CardContent>
-        <CardFooter className="flex justify-end gap-2">
+        <CardFooter className="flex justify-end gap-2 pt-6">
           <Button variant="outline">
             <Icons.LaptopMinimal className="mr-2 size-4" />
             Demo
