d60c49a1a788dad1568f45d419e3069d1bede61f
andreibojor <bojor.andrei@gmail.com>
2024-06-21 11:17:21 +0300

    dsfsdfdff

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -20,19 +20,99 @@ import {
 const Projects = () => {
   return (
     <>
-      <Carousel
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      {/* <Carousel
         opts={{
           align: 'start',
           loop: true,
         }}
-        className="w-full rounded-lg"
+        className="w-full max-w-xs rounded-lg"
       >
         <CarouselContent className="rounded-lg">
           {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem
-              key={index}
-              className="flex w-full gap-4 rounded-lg pt-4"
-            >
+            <CarouselItem key={index}>
+
+              <Card className="flex-col justify-between backdrop-blur supports-[backdrop-filter]:bg-background/70 md:w-1/2">
+                <CardHeader>
+                  <CardTitle>
+                    DevFlow{' '}
+                    <span className="text-3xl font-semibold">{index + 1}</span>
+                  </CardTitle>
+                  <CardDescription>
+                    A platform like StackOverflow. Deci imaginea siteului cu
+                    background gradient paralax intr-un card.
+                  </CardDescription>
+                </CardHeader>
+
+                <CardContent>
+                  <p>
+                    in loc de light dark ar putea sa fie ice fire si pe
+                    ice(light) sa fie albastrul deschis de la cyberpunk si pe
+                    dark sa fie cu rosu cyberpunk
+                  </p>
+                  <p>
+                    in loc de light dark ar putea sa fie ice fire si pe
+                    ice(light) sa fie albastrul deschis de la cyberpunk si pe
+                    dark sa fie cu rosu cyberpunk
+                  </p>
+                  <p>
+                    in loc de light dark ar putea sa fie ice fire si pe
+                    ice(light) sa fie albastrul deschis de la cyberpunk si pe
+                    dark sa fie cu rosu cyberpunk
+                  </p>
+                  <p>
+                    in loc de light dark ar putea sa fie ice fire si pe
+                    ice(light) sa fie albastrul deschis de la cyberpunk si pe
+                    dark sa fie cu rosu cyberpunk
+                  </p>
+                  <p>
+                    in loc de light dark ar putea sa fie ice fire si pe
+                    ice(light) sa fie albastrul deschis de la cyberpunk si pe
+                    dark sa fie cu rosu cyberpunk
+                  </p>
+                  <p>
+                    in loc de light dark ar putea sa fie ice fire si pe
+                    ice(light) sa fie albastrul deschis de la cyberpunk si pe
+                    dark sa fie cu rosu cyberpunk
+                  </p>
+                </CardContent>
+
+                <CardFooter className="flex justify-end gap-2 pt-6">
+                  <Link
+                    href="https://dev-flow-self.vercel.app"
+                    target="_blank"
+                    className="flex items-center rounded-sm border border-input bg-background px-4 py-2 hover:bg-accent hover:text-accent-foreground"
+                  >
+                    <Icons.LaptopMinimal className="mr-2 size-4" />
+                    Demo
+                  </Link>
+                  <Link
+                    href="https://github.com/Andreul369/DevFlow"
+                    target="_blank"
+                    className="flex items-center rounded-sm border border-input bg-background px-4 py-2 hover:bg-accent hover:text-accent-foreground"
+                  >
+                    <Icons.GitHub className="mr-2 size-4" />
+                    <span>Github</span>
+                  </Link>
+                </CardFooter>
+              </Card>
+
               <Card className="flex w-1/2 flex-grow flex-col justify-between backdrop-blur supports-[backdrop-filter]:bg-background/70">
                 <CardHeader>
                   <CardTitle>
@@ -114,7 +194,7 @@ const Projects = () => {
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
-      </Carousel>
+      </Carousel> */}
     </>
   );
 };
