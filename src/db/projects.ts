d62ae40e4621f79043f3c02edd1fa62acfaae1f6
andreibojor <bojor.andrei@gmail.com>
2024-07-12 20:09:35 +0300

    sdasdsd

--- src/db/projects.ts
+++ src/db/projects.ts
@@ -5,6 +5,6 @@ export const projects = [
     image: '/DevFlow.JPG',
     techStack: ['NextJS', 'MongoDB', 'Clerk', 'Tailwind', 'Shadcn'],
     description:
-      'A dynamic Q&A platform where users can log in with Clerk, post comments, add answers, vote on answers, favorite questions, search users and questions, and explore popular tags and recommended questions.',
+      'A dynamic Q&A platform similar to Stack Overflow. Users can log in with Clerk, ask questions and reply to others, vote on answers, favorite questions, search users or questions, and explore popular tags and recommended questions.',
   },
 ];
